b19e30f9be0499f4296dd3e218712b453a090ec1
Claas Rockmann-Buchterkirche <claas@rockbu.de>
2025-08-05 18:01:12 +0000

    pylint

--- unfold.py
+++ unfold.py
@@ -1,7 +1,9 @@
-from chess import pgn, Move
-from chess.pgn import Game, GameNode, ChildNode, Headers
-from typing import List
+"""Unfold a PGN file"""
+
 import argparse
+from typing import List
+from chess import pgn
+from chess.pgn import Game, GameNode
 
 parser = argparse.ArgumentParser()
 parser.add_argument('source', type=argparse.FileType('r'),
@@ -12,33 +14,34 @@ args = parser.parse_args()
 
 
 def recurse_game(node: GameNode) -> List[Game]:
-
+    """Recurse over game"""
     games = []
     if len(node.variations) > 0:
-        for i, v in enumerate(node.variations):
+        for _, v in enumerate(node.variations):
             games.extend(recurse_game(v))
     else:
-        g = pgn.Game()
-        g.headers = game.headers
+        temp_game = pgn.Game()
+        temp_game.headers = game.headers
         node_stack = []
         temp_node = node
         while temp_node.parent:
             node_stack.append(temp_node)
             temp_node = temp_node.parent
         node_stack.reverse()
-        node = g
+        node = temp_game
         for move_node in node_stack:
             node = node.add_variation(
-                move_node.move, comment=move_node.comment, nags=move_node.nags, starting_comment=move_node.starting_comment)
+                move_node.move, comment=move_node.comment, nags=move_node.nags,
+                starting_comment=move_node.starting_comment)
 
-        games.append(g)
+        games.append(temp_game)
     return games
 
 
 # pgn_file = open("testdata/test.pgn")
 game = pgn.read_game(args.source)
-games = recurse_game(game)
+game_list = recurse_game(game)
 
-for g in games:
+for g in game_list:
     print(g, file=args.target)
     print("", file=args.target)
